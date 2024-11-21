a31a152c72b80c2cf8974d153218e810677778ed
Lavee <iamalavityagi@gmail.com>
2024-11-22 01:17:41 +0530

    my first change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-		  Hi Lavi This is my first jenkins ci cd
+		  Hi Lavee 
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
